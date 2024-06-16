dbf74d9ac404010e2253b926c6ec9e57056dda84
Eduardo Borges <94413585+eduardoebot@users.noreply.github.com>
2024-06-16 14:12:38 -0300

    Conjuntos em Python
    Aprendizado sobre conjuntos em Python.

---
+++ Estudos/conjuntos.py
@@ -0,0 +1,7 @@
+#Conjuntos é não ordenado e que não permite dados repitidos. Não é possível realizar uma busca por index, devido ser não ordenado.
+
+nomes = {"Eduardo", "Gustavo", "Geovani"}
+nomes.add("Patrick")
+nomes.remove("Eduardo")
+
+print(nomes)
