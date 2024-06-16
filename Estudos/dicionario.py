caa17f61a835ecf24058e597e31ed9cbbdf16036
Eduardo Borges <94413585+eduardoebot@users.noreply.github.com>
2024-06-16 14:06:48 -0300

    Dicionário em Python
    Aprendizado sobre dicionário em Python

---
+++ Estudos/dicionario.py
@@ -0,0 +1,7 @@
+empresa = {
+    "TI": "Setor de Tecnologia da Informação.",
+    "Marketing": "Setor para divulgação e publicidade."
+}
+
+for setor in empresa:
+    print(setor, "é o ", empresa[setor])
