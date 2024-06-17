fa49e9ecb3f0a53c33ee6ea494809b4eaeabf408
Eduardo Borges <94413585+eduardoebot@users.noreply.github.com>
2024-06-17 09:26:16 -0300

    Novas funções
    Adicionado função de buscar contato específico, incluir novo contato e editar.

--- Estudos/agenda.py
+++ Estudos/agenda.py
@@ -1,5 +1,6 @@
 AGENDA = {}
 
+
 AGENDA["eduardo"] = {
     "telefone": "99999-8452",
     "endereco": "Rua A"
@@ -10,12 +11,39 @@ AGENDA["Maria"] = {
     "endereco": "Rua B"
 }
 
+
 def mostrar_contatos():
     for contato in AGENDA:
+        buscar_contato(contato)
+
+
+def buscar_contato(contato):
         print("_______________________________")
-        print(contato)
-        print(AGENDA[contato]['telefone'])
-        print(AGENDA[contato]['endereco'])
+        print("Nome:", contato)
+        print("Telefone:", AGENDA[contato]["telefone"])
+        print("Endereco:", AGENDA[contato]["endereco"])
         print("_______________________________")
 
-mostrar_contatos()
+
+def incluir_contato(nome, telefone, endereco):
+     AGENDA[nome] = {
+          "telefone": telefone,
+          "endereco": endereco,
+     }
+
+     print("Contato {} inserido com sucesso." .format(nome))
+
+
+def editar_contato(nome, telefone, endereco):
+     AGENDA[nome] = {
+          "telefone": telefone,
+          "endereco": endereco,
+     }
+
+     print("Contato {} editado com sucesso." .format(nome))
+
+incluir_contato("joao", "35659-4852", "Rua 65")
+#mostrar_contatos()
+buscar_contato("joao")
+editar_contato("joao", "12345-6789", "Rua nova")
+buscar_contato("joao")
