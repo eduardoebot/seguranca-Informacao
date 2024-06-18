90d1027617f168742c439db8c40eb7f721b5fe37
Eduardo Borges <94413585+eduardoebot@users.noreply.github.com>
2024-06-18 09:38:06 -0300

    Tratamento de erros
    Tratando possíveis erros utilizando try e except.

--- Estudos/agenda.py
+++ Estudos/agenda.py
@@ -13,25 +13,36 @@ AGENDA["Maria"] = {
 
 
 def mostrar_contatos():
-    for contato in AGENDA:
-        buscar_contato(contato)
+    if AGENDA:
+        for contato in AGENDA:
+            buscar_contato(contato)
+        
+    else:
+        print("Agenda vazia.")
 
 
 def buscar_contato(contato):
-        print("_______________________________")
-        print("Nome:", contato)
-        print("Telefone:", AGENDA[contato]["telefone"])
-        print("Endereco:", AGENDA[contato]["endereco"])
-        print("_______________________________")
+        try:
+            print("_______________________________")
+            print("Nome:", contato)
+            print("Telefone:", AGENDA[contato]["telefone"])
+            print("Endereco:", AGENDA[contato]["endereco"])
+            print("_______________________________")
+        
+        except:
+            print("Contato não cadastrado.")
 
 
-def incluir_editar_contato(nome, telefone, endereco):
-     AGENDA[nome] = {
-          "telefone": telefone,
-          "endereco": endereco,
+def incluir_editar_contato(nome):    
+    telefone = str(input("Digite o telefone."))
+    endereco = str(input("Digite o endereco."))
+
+    AGENDA[nome] = {
+        "telefone": telefone,
+        "endereco": endereco,
      }
-
-     print("Contato {} editado/inserido com sucesso." .format(nome))
+    
+    print("Operação realizada com sucesso.")
 
 
 def editar_contato(nome, telefone, endereco):
@@ -44,8 +55,15 @@ def editar_contato(nome, telefone, endereco):
 
 
 def excluir_contato(nome):
-     AGENDA.pop(nome)
-     print("{} excluido com sucesso." .format(nome))
+    try:
+        AGENDA.pop(nome)
+        print("{} excluido com sucesso." .format(nome))
+
+    except KeyError:
+        print("Contato não cadastrado.")
+
+    except:
+        print("Erro inesperado.")
 
 def menu():
     print("1 - Mostrar todos os contatos.")
@@ -58,8 +76,13 @@ def menu():
 
 while True:
     menu()
-    opcao = int(input("Digite a opção desejada."))
-    
+    try:
+        opcao = int(input("Digite a opção desejada."))
+        
+    except ValueError:
+        print("Opção inválida, digite um número.")
+        continue
+
     if opcao == 1:
         mostrar_contatos()
 
@@ -67,11 +90,24 @@ while True:
         contato = str(input("Digite o contato a ser localizado."))
         buscar_contato(contato)
     
-    elif opcao == 3 or opcao == 4:
-        nome = str(input("Digite o nome."))
-        telefone = str(input("Digite o telefone."))
-        endereco = str(input("Digite o endereco."))
-        incluir_editar_contato(nome, telefone, endereco)
+    elif opcao == 3:
+            nome = str(input("Digite o nome."))
+            try:
+                AGENDA[nome]
+                print("Contato já cadastrado")
+            except:
+                print("Inserindo novo contato: {}." .format(nome))
+                incluir_editar_contato(nome)
+
+    elif opcao == 4:
+            nome = str(input("Digite o nome."))
+            try:
+                AGENDA[nome]
+                print("Editando contato: {}." .format(nome))
+                incluir_editar_contato(nome)  
+            except:
+                print("Contato não cadastrado")
+
 
     elif opcao == 5:
         contato = str(input("Digite o contato a ser excluido"))
