9f8e9aa55c3a2cc39e9136f82512f27d9be3ab61
Eduardo Borges <94413585+eduardoebot@users.noreply.github.com>
2024-06-23 09:00:14 -0300

    Separação de pastas e script para fazer uma requisição

---
+++ Seguranca/client_tcp.py
@@ -0,0 +1,14 @@
+#Biblioteca que nos permite a fazer uma requisição. 
+import socket
+
+#AF_INET = IPV4
+#SOCK_STREAM = TCP / #SOCK_DGRAM = UDP
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(("google.com", 80))
+
+#Com o "b" converte string para bits
+client.send(b"GET / HTTP/1.1\nHost: www.google.com\n\n\n")
+
+#Converter bits para string e especificado para receber 1024bits.
+conteudo = client.recv(1024).decode()
+print(conteudo)
